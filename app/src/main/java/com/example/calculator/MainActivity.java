9182bd8cf43fa1e088fffc30e055e487a17b3e42
amits <amitsharmaproxy@gmail.com>
2021-06-19 19:51:46 +0530

    Calculator Application Update - 19:51 19-06-2021

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
@@ -19,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     TextView resultTV;
 
     String workings = "";
+    String formula = "";
+    String tempFormula = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,16 +46,26 @@ public class MainActivity extends AppCompatActivity {
         workingTV.setText("");
         workings = "";
         resultTV.setText("");
+        leftBracket = true;
     }
 
+    boolean leftBracket = true;
     public void bracketsOnClick(View view) {
+        if (leftBracket = true){
+            setWorkings("(");
+            leftBracket = false;
+        }else{
+            setWorkings("(");
+            leftBracket = true;
+        }
     }
 
     public void equalsOnClick(View view) {
         Double result = null;
         ScriptEngine engine = new ScriptEngineManager().getEngineByName("rhino");
+        checkForPower();
         try {
-            result = (double)engine.eval(workings);
+            result = (double)engine.eval(formula);
         } catch (ScriptException e) {
             Toast.makeText(this, "Invalid Input", Toast.LENGTH_SHORT).show();
         }
@@ -59,6 +74,54 @@ public class MainActivity extends AppCompatActivity {
             resultTV.setText(String.valueOf(result.doubleValue()));
         }
 
+    }
+
+    private void checkForPower() {
+        ArrayList<Integer> indexPowerOf = new ArrayList<>();
+        for (int i = 0; i<workings.length(); i++){
+            if (workings.charAt(i) == '^'){
+                indexPowerOf.add(i);
+            }
+            formula = workings;
+            tempFormula = workings;
+            for(Integer index:indexPowerOf){
+                changeFormula(index);
+            }
+            formula = tempFormula;
+        }
+    }
+
+    private boolean isNumeric(char c){
+        if ((c <= '9' && c >= '0') || c == '.'){
+            return true;
+        }
+        return false;
+    }
+
+    private void changeFormula(Integer index) {
+        String numberLeft = "";
+        String numberRight = "";
+
+        for (int i = index + 1; i < workings.length(); i++) {
+            if (isNumeric(workings.charAt(i))){
+                numberRight = numberRight + workings.charAt(i);
+
+            }else {
+                break;
+            }
+        }
+
+        for (int i = index - 1; i >= 0; i--) {
+            if (isNumeric(workings.charAt(i))){
+                numberLeft = numberLeft + workings.charAt(i);
+
+            }else {
+                break;
+            }
+        }
+        String original = numberLeft + "^" + numberRight;
+        String changed = "Math.pow(" + numberLeft + "," +  numberRight +")";
+        tempFormula = tempFormula.replace(original,changed);
     }
 
     public void sixOnClick(View view) {
