50e7f4cdf52db07cc1c5b78b187856f8f0e2fc0d
amits <amitsharmaproxy@gmail.com>
2021-06-19 17:39:06 +0530

    Calculator Application Update - 17:38 19-06-2021

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clearOnClear(View view) {
+    }
+
+    public void powerOfOnClick(View view) {
+    }
+
+    public void bracketsOnClick(View view) {
+    }
+
+    public void divisionOnClick(View view) {
+    }
+
+    public void sevenOnClick(View view) {
+    }
+
+    public void eightOnClick(View view) {
+    }
+
+    public void nineOnClick(View view) {
+    }
+
+    public void timesOnClick(View view) {
+    }
+
+    public void fourOnClick(View view) {
+    }
+
+    public void fiveOnClick(View view) {
+    }
+
+    public void minusOnClick(View view) {
+    }
+
+    public void oneOnClick(View view) {
+    }
+
+    public void twoOnClick(View view) {
+    }
+
+    public void threeOnClick(View view) {
+    }
+
+    public void plusOnClick(View view) {
+    }
+
+    public void decimalOnClick(View view) {
+    }
+
+    public void zeroOnClick(View view) {
+    }
+
+    public void equalsOnClick(View view) {
+    }
+
+    public void sixOnClick(View view) {
+    }
+}
