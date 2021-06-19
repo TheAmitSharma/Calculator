5957ac1c12bb990942fcd1b172acf43b5cd51806
amits <amitsharmaproxy@gmail.com>
2021-06-19 19:20:35 +0530

    Calculator Application Update - 19:20 19-06-2021

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -4,69 +4,125 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView workingTV;
+    TextView resultTV;
+
+    String workings = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initTextView();
+    }
+
+    private void initTextView() {
+        workingTV = (TextView)findViewById(R.id.workingTextView);
+        resultTV  = (TextView)findViewById(R.id.resultTextView);
+    }
+
+    private void setWorkings(String givenValue){
+        workings = workings + givenValue;
+        workingTV.setText(workings);
     }
 
     public void clearOnClear(View view) {
-    }
-
-    public void powerOfOnClick(View view) {
+        workingTV.setText("");
+        workings = "";
+        resultTV.setText("");
     }
 
     public void bracketsOnClick(View view) {
     }
 
-    public void divisionOnClick(View view) {
-    }
-
-    public void sevenOnClick(View view) {
-    }
-
-    public void eightOnClick(View view) {
-    }
-
-    public void nineOnClick(View view) {
-    }
-
-    public void timesOnClick(View view) {
-    }
-
-    public void fourOnClick(View view) {
-    }
-
-    public void fiveOnClick(View view) {
-    }
-
-    public void minusOnClick(View view) {
-    }
-
-    public void oneOnClick(View view) {
-    }
-
-    public void twoOnClick(View view) {
-    }
-
-    public void threeOnClick(View view) {
-    }
-
-    public void plusOnClick(View view) {
-    }
-
-    public void decimalOnClick(View view) {
-    }
-
-    public void zeroOnClick(View view) {
-    }
-
     public void equalsOnClick(View view) {
+        Double result = null;
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("rhino");
+        try {
+            result = (double)engine.eval(workings);
+        } catch (ScriptException e) {
+            Toast.makeText(this, "Invalid Input", Toast.LENGTH_SHORT).show();
+        }
+
+        if (result != null){
+            resultTV.setText(String.valueOf(result.doubleValue()));
+        }
+
     }
 
     public void sixOnClick(View view) {
+        setWorkings("6");
     }
+
+    public void powerOfOnClick(View view) {
+        setWorkings("^");
+    }
+
+    public void divisionOnClick(View view) {
+        setWorkings("/");
+    }
+
+    public void sevenOnClick(View view) {
+        setWorkings("7");
+    }
+
+    public void eightOnClick(View view) {
+        setWorkings("8");
+    }
+
+    public void nineOnClick(View view) {
+        setWorkings("9");
+    }
+
+    public void timesOnClick(View view) {
+        setWorkings("*");
+    }
+
+    public void fourOnClick(View view) {
+        setWorkings("4");
+    }
+
+    public void fiveOnClick(View view) {
+        setWorkings("5");
+    }
+
+    public void minusOnClick(View view) {
+        setWorkings("-");
+    }
+
+    public void oneOnClick(View view) {
+        setWorkings("1");
+    }
+
+    public void twoOnClick(View view) {
+        setWorkings("2");
+    }
+
+    public void threeOnClick(View view) {
+        setWorkings("3");
+    }
+
+    public void plusOnClick(View view) {
+        setWorkings("+");
+    }
+
+    public void decimalOnClick(View view) {
+        setWorkings(".");
+    }
+
+    public void zeroOnClick(View view) {
+        setWorkings("0");
+    }
+
 }
